82e7c852c8be67b120ad0d2b782e71a9eef8c3eb
michael-pessoni <pessoni94@gmail.com>
2022-05-10 16:36:30 -0300

    Create new test for ViewModel and LiveData

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -5,8 +5,8 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
 import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.Matchers.not
-import org.hamcrest.Matchers.nullValue
+import org.hamcrest.Matchers.*
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -16,15 +16,27 @@ class TasksViewModelTest{
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
 
+    private lateinit var tasksViewModel: TasksViewModel
+
+    @Before
+    fun setupViewModel() {
+        tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+    }
+
     @Test
     fun addNewTask_setsNewTaskEvent(){
-        val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
-
         tasksViewModel.addNewTask()
 
         val value = tasksViewModel.newTaskEvent.getOrAwaitValue()
 
         assertThat(value.getContentIfNotHandled(), (not(nullValue())))
+    }
+
+    @Test
+    fun setFilterAllTasks_tasksAddViewVisible(){
+        tasksViewModel.setFiltering(TasksFilterType.ALL_TASKS)
+
+        assertThat(tasksViewModel.tasksAddViewVisible.getOrAwaitValue(), `is`(true) )
     }
 
 }
